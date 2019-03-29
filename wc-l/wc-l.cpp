87680652a873725ef6ea1c8dc12e8ed2b58e2351
Welp- <minthourwnz@gmail.com>
2019-03-29 13:25:10 +0900

    task5

--- wc-l/wc-l.cpp
+++ wc-l/wc-l.cpp
@@ -10,10 +10,9 @@
 using namespace std;
 
 int main(int argc, char** argv ) {
-    long long t1, t2, freq, nread;
+    long long t1, t2, freq, count=0;
 	char str[4096];
-	long long count = 0;	
-	int i;
+	DWORD nread=1;
 	//if (argc == 1) 
     //{
     //    cerr << "Error: need text file\n";
@@ -22,22 +21,23 @@ int main(int argc, char** argv ) {
 	
 	cout << "Opening file a.txt\n"; // << argv[1] << "\n";
 	
-	FILE* f = fopen ("a.txt", "rb");
+	HANDLE f = CreateFile(L"a.txt", 0, 0, 0, OPEN_EXISTING, 0, 0);
 	
     QueryPerformanceFrequency((LARGE_INTEGER *)&freq);// запрашиваем число тиков в 1 сек
 
 
 	QueryPerformanceCounter((LARGE_INTEGER *)&t1);// смотрим время после окончания цикла
-	while (!feof(f)) {
-	  nread = fread (str, 1, 4096, f);
-	  for (i=0; i<nread; i++){
+	BOOL bResult = TRUE;
+	while (!(bResult && nread == 0)) {
+	  bResult=ReadFile(f, str, 4096, &nread, 0);
+	  for (DWORD i=0; i<nread; i++){
 		if(str[i]=='\n')
 			count++;
 	  }
 	}
 	QueryPerformanceCounter((LARGE_INTEGER *)&t2);// смотрим время после окончания цикла
 	
-	fclose(f);
+	CloseHandle(f);
 
 	cout << count << "\n Time spent:" << (t2-t1)/(1.*freq);
 	return 0;
